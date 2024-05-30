feebc03b27729699426561ee10ae0283f4fdea91
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-30 17:29:11 +0200

    add pivot

--- database/seeders/FlavoursTableSeeder.php
+++ database/seeders/FlavoursTableSeeder.php
@@ -13,6 +13,15 @@ class FlavoursTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-    }
+        $json = file_get_contents('database/externaldata/aromi-vini.json');
+        $data=json_decode($json, true);
+        $flavours = $data['aromi'];
+
+
+        foreach ($flavours as $flavour){
+            $new_flavour = new Flavour();
+            $new_flavour->aroma=$flavour;
+            $new_flavour->save();
+        }
+}
 }
